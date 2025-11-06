d85c437116dae379b2e7d07d4947e6abc60a2952
minnnnnnni <dkstodtjdals@gmail.com>
2025-11-06 18:01:45 +0900

    Feat: 커스텀 예외처리 정의

--- src/main/java/com/plink/backend/global/exception/CustomException.java
+++ src/main/java/com/plink/backend/global/exception/CustomException.java
@@ -1,10 +1,8 @@
-package com.plink.backend.exception;
+package com.plink.backend.global.exception;
 
 import org.springframework.http.HttpStatus;
 
-/**
- * 모든 비즈니스 예외에서 공통적으로 사용할 커스텀 예외 클래스
- */
+// 공통으로 사용하는 커스텀 예외 클래스
 public class CustomException extends RuntimeException {
     private final HttpStatus status;
 
--- src/main/java/com/plink/backend/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/plink/backend/global/exception/GlobalExceptionHandler.java
@@ -1,4 +1,29 @@
 package com.plink.backend.global.exception;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+// 프로젝트 전역 예외 핸들러
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    // 커스텀 예외 처리하는 핸들러
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<Map<String, String>> handleCustomException(CustomException ex) {
+        return ResponseEntity
+                .status(ex.getStatus())
+                .body(Map.of("message", ex.getMessage()));
+    }
+
+    // 예상치 못한 예외 처리하는 로직
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, String>> handleGeneralException(Exception ex) {
+        ex.printStackTrace(); // 로그 확인용
+        return ResponseEntity
+                .internalServerError()
+                .body(Map.of("message", "서버 내부 오류가 발생했습니다."));
+    }
 }
