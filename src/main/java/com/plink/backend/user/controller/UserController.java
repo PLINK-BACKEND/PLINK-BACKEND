98279a5b61966e5ff177541724d4953c4fde4aec
minnnnnnni <dkstodtjdals@gmail.com>
2025-11-13 13:51:05 +0900

    Feat: 닉네임 축제 전역으로 중복불하

--- src/main/java/com/plink/backend/user/controller/UserController.java
+++ src/main/java/com/plink/backend/user/controller/UserController.java
@@ -55,7 +55,7 @@ public class UserController {
             @RequestParam String slug,
             @RequestParam String nickname
     ) {
-        boolean exists = userFestivalRepository.existsByNicknameAndFestivalSlug(nickname, slug);
+        boolean exists = userFestivalRepository.existsByNickname(nickname);
 
         if (exists) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body("중복된 닉네임입니다.");
