c000f683d47bfca0b1929b08fed1ba75526b659a
minnnnnnni <dkstodtjdals@gmail.com>
2025-11-13 13:56:29 +0900

    Feat: /user/로 시작하는 엔드포인트는 전부 접근 허용

--- src/main/java/com/plink/backend/config/SecurityConfig.java
+++ src/main/java/com/plink/backend/config/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/auth/**",        // 로그인, 회원가입, 게스트
-                                "/user/info",     // 로그인한 유저 조회용
+                                "/user/**",     // 로그인한 유저 조회용
                                 "/ws/**",         // 웹소켓 엔드포인트
                                 "/error",           // 오류 페이지 등등
                                 "/*/games/**",
