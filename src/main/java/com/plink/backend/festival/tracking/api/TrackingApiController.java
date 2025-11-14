9ea266230f37b18984abb91c2a4942f9dbeb9ae1
seoah <24seoah@sookmyung.ac.kr>
2025-11-14 12:16:27 +0900

    Change API endpoint

--- src/main/java/com/plink/backend/festival/tracking/api/TrackingApiController.java
+++ src/main/java/com/plink/backend/festival/tracking/api/TrackingApiController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/plink/festival")
+@RequestMapping("/plink/festivals")
 @RequiredArgsConstructor
 
 public class TrackingApiController {
